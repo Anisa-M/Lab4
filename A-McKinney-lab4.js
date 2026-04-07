d3b52e6cbf2e8c705433c16f822010bbe578a1b8
Anisa McKinney <amckinney3@hawk.illinoistech.edu>
2026-04-06 21:10:08 -0500

    Updating code

--- A-McKinney-lab4.js
+++ A-McKinney-lab4.js
@@ -8,28 +8,27 @@
   //  headLine = "Uplift Your Brand with Stellar Marketing"; 
   // 1b. Change the line of text below the main headline
     const underText = document.querySelector(".text-xl mb-6 text-white relative z-20").innerHTML = "Utilize cutting-edge strategies from Stellar Marketing to help your business<strong> thrive </strong> and <strong> excel. </strong>";
-   // underText = "Utilize cutting-edge strategies from Stellar Marketing to help your business<strong> thrive <strong/> and <strong> excel. </strong>"; 
   // 1c. Change the background image 
     const backImage = document.querySelector(".text-xl font-semibold mb-2")
-    backImage = "https://picsum.photos/id/683/1280/720"; 
+    element.style.backImage = "https://picsum.photos/id/683/1280/720"; 
   // 1d. Remove "get started"
     const removeGetStarted = document.querySelector(".relative bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 z-20");
-    removeGetStarted = ""; 
-  // 1e. Change the background color
-     const backcolor = document.querySelector("");
-     backcolor.style.color = ""; 
-
+    removeGetStarted.remove(); 
+  // 1e. Change the background color of nav bar
+     const backcolor = document.querySelector(".bg-gray-800 text-white p-4 mt-12");
+     const style = window.getComputedStyle(backcolor); 
+     const nav = document.querySelector("");
   // 2a. Change the icons color
     const iconsColor = document.querySelector(".material-symbols-outlined text-6xl");
     iconsColor.backgroundColor = green; 
   // 2b. Change the digital marketing icon
     const digMarketing = document.querySelector("[data-icon='brand']");
-    digMarketing.innerText = "ads_click:FILL@0;wght@400:GRAD@0;opsz@24&icon.size=24&icon.color=%23e8eaed"; 
+    digMarketing.innerText = "ads_click"; 
   // 3a. Change the layout of the tiles
     const tileLayout = document.querySelector(".relative product_card rounded-lg overflow-hidden");
     tileLayout.style.size = "20px";
   // 3b. Change the Musicians image
     const musiciansImage = document.querySelector("#solutions .product_card:last-child img");
-    musiciansImage = "https://picsum.photos/id/453/400/300"; 
+    musiciansImage.src = "https://picsum.photos/id/453/400/300"; 
     
 })();
