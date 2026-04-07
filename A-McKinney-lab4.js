e63515f86aae25ca5511ac9ed6a0583d8896b651
Anisa McKinney <amckinney3@hawk.illinoistech.edu>
2026-04-06 21:27:21 -0500

    Fixing query syntax

--- A-McKinney-lab4.js
+++ A-McKinney-lab4.js
@@ -4,28 +4,28 @@
 (function () {
   // statements…
   // 1a. Change the main headline
-    document.querySelector(".text-4xl font-bold mb-4 text-white relative z-20").innerHTML = "Uplift Your Brand with Stellar Marketing"; 
+    document.querySelector(".text-4xl.font-bold.mb-4.text-white.relative.z-20").innerHTML = "Uplift Your Brand with Stellar Marketing"; 
   //  headLine = "Uplift Your Brand with Stellar Marketing"; 
   // 1b. Change the line of text below the main headline
-    const underText = document.querySelector(".text-xl mb-6 text-white relative z-20").innerHTML = "Utilize cutting-edge strategies from Stellar Marketing to help your business<strong> thrive </strong> and <strong> excel. </strong>";
+    const underText = document.querySelector(".text-xl.mb-6.text-white.relative.z-20").innerHTML = "Utilize cutting-edge strategies from Stellar Marketing to help your business<strong> thrive </strong> and <strong> excel. </strong>";
   // 1c. Change the background image 
-    const backImage = document.querySelector(".text-xl font-semibold mb-2")
+    const backImage = document.querySelector(".text-xl.font-semibold.mb-2")
     element.style.backImage = "https://picsum.photos/id/683/1280/720"; 
   // 1d. Remove "get started"
-    const removeGetStarted = document.querySelector(".relative bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 z-20");
+    const removeGetStarted = document.querySelector(".relative.bg-blue-600.text-white.px-6.py-2.rounded.hover:bg-blue-700.z-20");
     removeGetStarted.remove(); 
   // 1e. Change the background color of nav bar
-     const backcolor = document.querySelector(".bg-gray-800 text-white p-4 mt-12");
+     const backcolor = document.querySelector(".bg-gray-800.text-white.p-4.mt-12");
      const style = window.getComputedStyle(backcolor); 
      const nav = document.querySelector("");
   // 2a. Change the icons color
-    const iconsColor = document.querySelector(".material-symbols-outlined text-6xl");
+    const iconsColor = document.querySelector(".material-symbols-outlined.text-6xl");
     iconsColor.backgroundColor = green; 
   // 2b. Change the digital marketing icon
     const digMarketing = document.querySelector("[data-icon='brand']");
     digMarketing.innerText = "ads_click"; 
   // 3a. Change the layout of the tiles
-    const tileLayout = document.querySelector(".relative product_card rounded-lg overflow-hidden");
+    const tileLayout = document.querySelector(".relative.product_card.rounded-lg.overflow-hidden");
     tileLayout.style.size = "20px";
   // 3b. Change the Musicians image
     const musiciansImage = document.querySelector("#solutions .product_card:last-child img");
